bb949b623ec2610b390434c9c7d67dcab723c489
Askarii <muhammadaskari267@gmail.com>
2021-02-22 00:03:53 +0500

    Forget Passwprd

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import Forget from './components/login/Forget';
+import Login from './components/login/Login';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import 'antd/dist/antd.css';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Login />
+        <Route path='/forget_password' component={Forget} />
+      </Router>
     </div>
   );
 }
