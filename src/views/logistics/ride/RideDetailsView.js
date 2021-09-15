6e07fcdd3f74228029243d3b91ff4b52dd9fdb40
HunbalSiddiqui <hunbal.dha@gmail.com>
2021-09-15 14:21:19 +0500

    Routes init.

--- src/views/logistics/ride/RideDetailsView.js
+++ src/views/logistics/ride/RideDetailsView.js
@@ -16,19 +16,7 @@ import {
 function RideDetailsView({ selectedRide, open, handleClose }) {
     return (
         <div style={{ display: "inline" }}>
-            <form>
-                <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
-                    <DialogTitle>
-                        View Ride
-                    </DialogTitle>
-                    <DialogContent>
-                        {/* TODO: will add textfields once apis are ready */}
-                    </DialogContent>
-                    <DialogActions>
-                        <Button onClick={handleClose} color="default" variant="contained">Close</Button>
-                    </DialogActions>
-                </Dialog>
-            </form>
+
         </div>
     )
 }
