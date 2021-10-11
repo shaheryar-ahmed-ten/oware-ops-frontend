c0f66515fc9bdc98c53dd8ad8e7e414c4b601eef
Shaheryar Ahmed <shaheryarahmed@pop-os.localdomain>
2021-10-11 11:29:02 +0500

    bulk upload missing button in headers added

--- src/views/administration/product/ProductView.js
+++ src/views/administration/product/ProductView.js
@@ -1,4 +1,4 @@
-import { useEffect, useState, useCallback } from 'react';
+import { useEffect, useState, useCallback } from "react";
 import {
   makeStyles,
   Paper,
@@ -10,108 +10,121 @@ import {
   TableCell,
   TableContainer,
   TableHead,
-  TableRow
-} from '@material-ui/core';
-import TableHeader from '../../../components/TableHeader'
-import axios from 'axios';
-import { getURL, digitize } from '../../../utils/common';
-import { Alert, Pagination } from '@material-ui/lab';
-import EditIcon from '@material-ui/icons/EditOutlined';
-import DeleteIcon from '@material-ui/icons/DeleteOutlined';
-import ConfirmDelete from '../../../components/ConfirmDelete';
-import AddProductView from './AddProductView';
-import { debounce } from 'lodash';
-import { DEBOUNCE_CONST } from '../../../Config';
-import MessageSnackbar from '../../../components/MessageSnackbar';
-import ProductsCsvReader from '../../../components/ProductsCsvReader';
-import { useNavigate } from 'react-router';
+  TableRow,
+} from "@material-ui/core";
+import TableHeader from "../../../components/TableHeader";
+import axios from "axios";
+import { getURL, digitize } from "../../../utils/common";
+import { Alert, Pagination } from "@material-ui/lab";
+import EditIcon from "@material-ui/icons/EditOutlined";
+import DeleteIcon from "@material-ui/icons/DeleteOutlined";
+import ConfirmDelete from "../../../components/ConfirmDelete";
+import AddProductView from "./AddProductView";
+import { debounce } from "lodash";
+import { DEBOUNCE_CONST } from "../../../Config";
+import MessageSnackbar from "../../../components/MessageSnackbar";
+import ProductsCsvReader from "../../../components/ProductsCsvReader";
+import { useNavigate } from "react-router";
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles((theme) => ({
   root: {
-    width: '100%',
-    marginBottom: '20px'
+    width: "100%",
+    marginBottom: "20px",
   },
   container: {
     // maxHeight: 450,
     padding: 20,
   },
   active: {
-    color: theme.palette.success.main
+    color: theme.palette.success.main,
   },
   searchInput: {
-    border: '1px solid grey',
+    border: "1px solid grey",
     borderRadius: 4,
     opacity: 0.6,
-    padding: '0px 8px',
+    padding: "0px 8px",
     marginRight: 7,
     height: 30,
   },
 }));
 
-
 export default function ProductView() {
   const classes = useStyles();
-  const columns = [{
-    id: 'id',
-    label: 'PRODUCT ID',
-    minWidth: 'auto',
-    className: '',
-    format: value => digitize(value, 6)
-  }, {
-    id: 'name',
-    label: 'Name',
-    minWidth: 'auto',
-    className: '',
-  }, {
-    id: 'description',
-    label: 'Description',
-    minWidth: 'auto',
-    className: '',
-  }, {
-    id: 'dimensionsCBM',
-    label: <span>Volume cm<sup>3</sup></span>,
-    minWidth: 'auto',
-    className: '',
-  }, {
-    id: 'weight',
-    label: 'Weight in KGs',
-    minWidth: 'auto',
-    className: '',
-  }, {
-    id: 'Category.name',
-    label: 'Category',
-    minWidth: 'auto',
-    className: '',
-    format: (value, entity) => entity.Category.name
-  }, {
-    id: 'Brand.name',
-    label: 'Brand',
-    minWidth: 'auto',
-    className: '',
-    format: (value, entity) => entity.Brand.name
-  }, {
-    id: 'UOM.name',
-    label: 'UoM',
-    minWidth: 'auto',
-    className: '',
-    format: (value, entity) => entity.UOM.name
-  }, {
-    id: 'isActive',
-    label: 'Status',
-    minWidth: 'auto',
-    className: value => value ? classes.active : '',
-    format: value => value ? 'Active' : 'In-Active',
-  }, {
-    id: 'actions',
-    label: '',
-    minWidth: 'auto',
-    className: '',
-    format: (value, entity) =>
-      [
+  const columns = [
+    {
+      id: "id",
+      label: "PRODUCT ID",
+      minWidth: "auto",
+      className: "",
+      format: (value) => digitize(value, 6),
+    },
+    {
+      id: "name",
+      label: "Name",
+      minWidth: "auto",
+      className: "",
+    },
+    {
+      id: "description",
+      label: "Description",
+      minWidth: "auto",
+      className: "",
+    },
+    {
+      id: "dimensionsCBM",
+      label: (
+        <span>
+          Volume cm<sup>3</sup>
+        </span>
+      ),
+      minWidth: "auto",
+      className: "",
+    },
+    {
+      id: "weight",
+      label: "Weight in KGs",
+      minWidth: "auto",
+      className: "",
+    },
+    {
+      id: "Category.name",
+      label: "Category",
+      minWidth: "auto",
+      className: "",
+      format: (value, entity) => entity.Category.name,
+    },
+    {
+      id: "Brand.name",
+      label: "Brand",
+      minWidth: "auto",
+      className: "",
+      format: (value, entity) => entity.Brand.name,
+    },
+    {
+      id: "UOM.name",
+      label: "UoM",
+      minWidth: "auto",
+      className: "",
+      format: (value, entity) => entity.UOM.name,
+    },
+    {
+      id: "isActive",
+      label: "Status",
+      minWidth: "auto",
+      className: (value) => (value ? classes.active : ""),
+      format: (value) => (value ? "Active" : "In-Active"),
+    },
+    {
+      id: "actions",
+      label: "",
+      minWidth: "auto",
+      className: "",
+      format: (value, entity) => [
         <EditIcon key="edit" onClick={() => openEditView(entity)} />,
         // <DeleteIcon color="error" key="delete" onClick={() => openDeleteView(entity)} />
-      ]
-  }];
+      ],
+    },
+  ];
   const [pageCount, setPageCount] = useState(1);
   const [page, setPage] = useState(1);
   const [products, setProducts] = useState([]);
@@ -120,106 +133,119 @@ export default function ProductView() {
   const [uoms, setUoms] = useState([]);
   const [categories, setCategories] = useState([]);
 
-  const [searchKeyword, setSearchKeyword] = useState('');
+  const [searchKeyword, setSearchKeyword] = useState("");
   const [selectedProduct, setSelectedProduct] = useState(null);
-  const [formErrors, setFormErrors] = useState('');
+  const [formErrors, setFormErrors] = useState("");
   const [addProductViewOpen, setAddProductViewOpen] = useState(false);
   const [deleteProductViewOpen, setDeleteProductViewOpen] = useState(false);
-  const [showMessage, setShowMessage] = useState(null)
+  const [showMessage, setShowMessage] = useState(null);
   const [messageType, setMessageType] = useState(null);
 
   const navigate = useNavigate();
 
-  const addProduct = data => {
+  const addProduct = (data) => {
     let apiPromise = null;
-    if (!selectedProduct) apiPromise = axios.post(getURL('product'), data);
+    if (!selectedProduct) apiPromise = axios.post(getURL("product"), data);
     else apiPromise = axios.put(getURL(`product/${selectedProduct.id}`), data);
-    apiPromise.then(res => {
+    apiPromise.then((res) => {
       if (!res.data.success) {
-        setFormErrors(<Alert elevation={6} variant="filled" severity="error" onClose={() => setFormErrors('')}>{res.data.message}</Alert>);
-        return
+        setFormErrors(
+          <Alert elevation={6} variant="filled" severity="error" onClose={() => setFormErrors("")}>
+            {res.data.message}
+          </Alert>
+        );
+        return;
       }
       setShowMessage({
-        message: "New product has been created."
-      })
+        message: "New product has been created.",
+      });
       closeAddProductView(false);
       getProducts();
     });
   };
 
-  const bulkUpload = data => {
-    let apiPromise = axios.post(getURL('product/bulk'), data)
-    apiPromise.then((res) => {
-      if (!res.data.success) {
-        setFormErrors(<Alert elevation={6} variant="filled" severity="error" onClose={() => setFormErrors('')}>{res.data.message}</Alert>);
-        return
-      }
-      setMessageType('green')
-      setShowMessage({
-        message: "New products has been created."
-      })
-      closeAddProductView(false);
-      getProducts();
-    })
-      .catch((err) => {
-        setMessageType('#FFCC00')
-        setShowMessage({
-          message: `${err.response.data.message || 'Invalid file data.'}`
-        })
-      })
-  }
-
-  const deleteProduct = data => {
-    axios.delete(getURL(`product / ${selectedProduct.id}`))
-      .then(res => {
+  const bulkUpload = (data) => {
+    let apiPromise = axios.post(getURL("product/bulk"), data);
+    apiPromise
+      .then((res) => {
         if (!res.data.success) {
-          setFormErrors(<Alert elevation={6} variant="filled" severity="error" onClose={() => setFormErrors('')}>{res.data.message}</Alert>);
-          return
+          setFormErrors(
+            <Alert elevation={6} variant="filled" severity="error" onClose={() => setFormErrors("")}>
+              {res.data.message}
+            </Alert>
+          );
+          return;
         }
-        closeDeleteProductView();
+        setMessageType("green");
+        setShowMessage({
+          message: "New products has been created.",
+        });
+        closeAddProductView(false);
         getProducts();
+      })
+      .catch((err) => {
+        setMessageType("#FFCC00");
+        setShowMessage({
+          message: `${err.response.data.message || "Invalid file data."}`,
+        });
       });
   };
 
-  const openEditView = product => {
+  const deleteProduct = (data) => {
+    axios.delete(getURL(`product / ${selectedProduct.id}`)).then((res) => {
+      if (!res.data.success) {
+        setFormErrors(
+          <Alert elevation={6} variant="filled" severity="error" onClose={() => setFormErrors("")}>
+            {res.data.message}
+          </Alert>
+        );
+        return;
+      }
+      closeDeleteProductView();
+      getProducts();
+    });
+  };
+
+  const openEditView = (product) => {
     setSelectedProduct(product);
     setAddProductViewOpen(true);
-  }
+  };
 
-  const openDeleteView = product => {
+  const openDeleteView = (product) => {
     setSelectedProduct(product);
     setDeleteProductViewOpen(true);
-  }
+  };
 
   const closeAddProductView = () => {
     setSelectedProduct(null);
     setAddProductViewOpen(false);
-  }
+  };
 
   const closeDeleteProductView = () => {
     setSelectedProduct(null);
     setDeleteProductViewOpen(false);
-  }
+  };
 
   const _getProducts = (page, searchKeyword) => {
-    axios.get(getURL('product'), { params: { page, search: searchKeyword } })
-      .then(res => {
-        setPageCount(res.data.pages)
-        setProducts(res.data.data)
-      });
-  }
+    axios.get(getURL("product"), { params: { page, search: searchKeyword } }).then((res) => {
+      setPageCount(res.data.pages);
+      setProducts(res.data.data);
+    });
+  };
 
-  const getProducts = useCallback(debounce((page, searchKeyword) => {
-    _getProducts(page, searchKeyword);
-  }, DEBOUNCE_CONST), []);
+  const getProducts = useCallback(
+    debounce((page, searchKeyword) => {
+      _getProducts(page, searchKeyword);
+    }, DEBOUNCE_CONST),
+    []
+  );
 
   const getRelations = () => {
-    axios.get(getURL('product/relations'))
-      .then(res => {
-        setBrands(res.data.brands)
-        setUoms(res.data.uoms)
-        setCategories(res.data.categories)
-      });
+    axios.get(getURL("product/relations")).then((res) => {
+      setBrands(res.data.brands);
+      setUoms(res.data.uoms);
+      setCategories(res.data.categories);
+    });
   };
 
   useEffect(() => {
@@ -230,55 +256,65 @@ export default function ProductView() {
     getRelations();
   }, []);
 
-  const searchInput = <InputBase
-    placeholder="Search"
-    className={classes.searchInput}
-    id="search"
-    label="Search"
-    type="text"
-    variant="outlined"
-    value={searchKeyword}
-    key={1}
-    onChange={e => setSearchKeyword(e.target.value)}
-  />;
+  const searchInput = (
+    <InputBase
+      placeholder="Search"
+      className={classes.searchInput}
+      id="search"
+      label="Search"
+      type="text"
+      variant="outlined"
+      value={searchKeyword}
+      key={1}
+      onChange={(e) => setSearchKeyword(e.target.value)}
+    />
+  );
 
   // const addBulkProductsButton = <ProductsCsvReader bulkUpload={bulkUpload} />;
-  const addBulkProductsButton = <Button
-    key={4}
-    variant="contained"
-    color="primary"
-    size="small"
-    style={{ width: 150, transform: 'translateX(7px)' }}
-    onClick={() => navigate('bulk-upload')}>Bulk Upload</Button>;
+  const addBulkProductsButton = (
+    <Button
+      key={4}
+      variant="contained"
+      color="primary"
+      size="small"
+      style={{ width: 150, transform: "translateX(7px)" }}
+      onClick={() => navigate("bulk-upload")}
+    >
+      Bulk Upload
+    </Button>
+  );
 
+  const addProductButton = (
+    <Button key={2} variant="contained" color="primary" size="small" onClick={() => setAddProductViewOpen(true)}>
+      ADD PRODUCT
+    </Button>
+  );
 
-  const addProductButton = <Button
-    key={2}
-    variant="contained"
-    color="primary"
-    size="small"
-    onClick={() => setAddProductViewOpen(true)}>ADD PRODUCT</Button>;
+  const addProductModal = (
+    <AddProductView
+      formErrors={formErrors}
+      key={3}
+      brands={brands}
+      uoms={uoms}
+      categories={categories}
+      selectedProduct={selectedProduct}
+      open={addProductViewOpen}
+      addProduct={addProduct}
+      handleClose={() => closeAddProductView()}
+    />
+  );
 
-  const addProductModal = <AddProductView
-    formErrors={formErrors}
-    key={3}
-    brands={brands}
-    uoms={uoms}
-    categories={categories}
-    selectedProduct={selectedProduct}
-    open={addProductViewOpen}
-    addProduct={addProduct}
-    handleClose={() => closeAddProductView()} />
-
-  const deleteProductModal = <ConfirmDelete
-    key={4}
-    confirmDelete={deleteProduct}
-    open={deleteProductViewOpen}
-    handleClose={closeDeleteProductView}
-    selectedEntity={selectedProduct && selectedProduct.name}
-    title={"Product"}
-  />
-  const headerButtons = [searchInput, addProductButton, addProductModal, deleteProductModal];
+  const deleteProductModal = (
+    <ConfirmDelete
+      key={4}
+      confirmDelete={deleteProduct}
+      open={deleteProductViewOpen}
+      handleClose={closeDeleteProductView}
+      selectedEntity={selectedProduct && selectedProduct.name}
+      title={"Product"}
+    />
+  );
+  const headerButtons = [searchInput, addProductButton, addBulkProductsButton, addProductModal, deleteProductModal];
 
   return (
     <Paper className={classes.root}>
@@ -291,7 +327,12 @@ export default function ProductView() {
                 <TableCell
                   key={column.id}
                   align={column.align}
-                  style={{ minWidth: column.minWidth, background: 'transparent', fontWeight: 'bolder', fontSize: '12px' }}
+                  style={{
+                    minWidth: column.minWidth,
+                    background: "transparent",
+                    fontWeight: "bolder",
+                    fontSize: "12px",
+                  }}
                 >
                   {column.label}
                 </TableCell>
@@ -305,8 +346,15 @@ export default function ProductView() {
                   {columns.map((column) => {
                     const value = product[column.id];
                     return (
-                      <TableCell key={column.id} align={column.align}
-                        className={column.className && typeof column.className === 'function' ? column.className(value) : column.className}>
+                      <TableCell
+                        key={column.id}
+                        align={column.align}
+                        className={
+                          column.className && typeof column.className === "function"
+                            ? column.className(value)
+                            : column.className
+                        }
+                      >
                         {column.format ? column.format(value, product) : value}
                       </TableCell>
                     );
@@ -328,7 +376,7 @@ export default function ProductView() {
             page={page}
             className={classes.pagination}
             onChange={(e, page) => setPage(page)}
-          // onChangeRowsPerPage={handleChangeRowsPerPage}
+            // onChangeRowsPerPage={handleChangeRowsPerPage}
           />
         </Grid>
       </Grid>
