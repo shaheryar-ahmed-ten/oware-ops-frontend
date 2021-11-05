a9ecf74c049c1ecc87628ed4c71340aa4e50d023
M.Hunbal Siddiqui - PivotalWorks <hunbal.siddiqui@pivotalworks.io>
2021-11-05 17:39:57 +0500

    Export date filter added on dispatch.

--- src/views/operations/dispatchOrder/DispatchOrderView.js
+++ src/views/operations/dispatchOrder/DispatchOrderView.js
@@ -14,10 +14,18 @@ import {
   Backdrop,
   Typography,
   Tooltip,
+  InputAdornment,
+  MenuItem,
+  FormControl,
+  Select,
+  TextField,
+  ListItem,
+  DialogTitle,
+  Dialog,
 } from "@material-ui/core";
 import TableHeader from "../../../components/TableHeader";
 import axios from "axios";
-import { getURL, dateFormat } from "../../../utils/common";
+import { getURL, dateFormat, dividerDateFormatForFilter } from "../../../utils/common";
 import { Alert, Pagination } from "@material-ui/lab";
 import ConfirmDelete from "../../../components/ConfirmDelete";
 import { debounce } from "lodash";
@@ -32,6 +40,7 @@ import SelectDropdown from '../../../components/SelectDropdown';
 import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
 import moment from 'moment-timezone';
 import FileDownload from 'js-file-download';
+import CalendarTodayOutlinedIcon from '@material-ui/icons/CalendarTodayOutlined';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -103,7 +112,15 @@ const useStyles = makeStyles((theme) => ({
   },
   exportBtn: {
     marginLeft: 5
-  }
+  },
+  placeholderText: {
+    color: "#CAC9C9",
+    '& .MuiSelect-outlined': {
+      paddingTop: '6px',
+      paddingBottom: '6px',
+    },
+    marginLeft: 5
+  },
 }));
 
 export default function DispatchOrderView() {
@@ -220,12 +237,6 @@ export default function DispatchOrderView() {
       minWidth: 150,
       className: "",
       format: (value, entity) => {
-        // let totalDispatched = 0
-        // entity.ProductOutwards.forEach(po => {
-        //   po.OutwardGroups.forEach(outGroup => {
-        //     totalDispatched += outGroup.quantity
-        //   });
-        // });
         return [
           <VisibilityIcon key="view" onClick={() => navigate(`view/${entity.id}`)} style={{ cursor: "pointer" }} />,
           entity.status != 3 ? (
@@ -299,6 +310,27 @@ export default function DispatchOrderView() {
   ])
   const [selectedFilterStatus, setSelectedFilterStatus] = useState(null)
 
+  const [startDate, setStartDate] = useState(null)
+  const [endDate, setEndDate] = useState(null)
+  const [trackDateFilterOpen, setTrackDateFilterOpen] = useState(false)
+  const [days] = useState([{
+    id: 7,
+    name: '7 days'
+  }, {
+    id: 14,
+    name: '14 days'
+  }, {
+    id: 30,
+    name: '30 days'
+  }, {
+    id: 60,
+    name: '60 days'
+  }])
+  const [selectedDay, setSelectedDay] = useState(null)
+  const [openDialog, setOpenDialog] = useState(false)
+  const [selectedDateRange, setSelectedDateRange] = useState(false) // bool
+  const [reRender, setReRender] = useState(false)
+
   const cancelDispatchOrder = (dispatchOrderId) => {
     axios
       .patch(getURL(`dispatch-order/cancel/${dispatchOrderId}`))
@@ -359,10 +391,13 @@ export default function DispatchOrderView() {
   }, [page, searchKeyword, selectedFilterStatus]);
 
   const exportToExcel = () => {
+    let startingDate = new Date(startDate);
+    let endingDate = new Date(endDate);
+    
     axios.get(getURL('dispatch-order/export'), {
       responseType: 'blob',
       params: {
-        page, search: searchKeyword
+        page, search: searchKeyword, days: !selectedDateRange ? selectedDay : null, startingDate: selectedDateRange ? startingDate : null, endingDate: selectedDateRange ? endingDate : null
         ,
         client_Tz: moment.tz.guess()
       },
@@ -438,11 +473,91 @@ export default function DispatchOrderView() {
     onClick={() => exportToExcel()}
   > EXPORT TO EXCEL</Button >;
 
+  const handleCloseDialog = () => {
+    setOpenDialog(false);
+  }
+
+  const handleChange = (event) => {
+    // reset the local state to remove the helper text
+    if (event.target.value !== 'custom' && selectedDateRange) {
+      setSelectedDateRange(false)
+    }
+    setPage(1)
+    setSelectedDay(event.target.value);
+  };
+
+  const daysSelect = <FormControl className={classes.formControl}>
+    <Select
+      value={selectedDay}
+      variant="outlined"
+      onChange={handleChange}
+      displayEmpty
+      inputProps={{ 'aria-label': 'Without label' }}
+      className={classes.placeholderText}
+      startAdornment={
+        <InputAdornment position="start" classes={{ positionStart: classes.inputAdronmentStyle, root: classes.inputAdronmentStyle }}>
+          <CalendarTodayOutlinedIcon fontSize="small" />
+        </InputAdornment>
+      }
+      onOpen={() => setTrackDateFilterOpen(true)}
+      onClose={() => setTrackDateFilterOpen(false)}
+    >
+      <MenuItem value={null} disabled>
+        <span className={classes.dropdownListItem}>Select Days</span>
+      </MenuItem>
+      {
+        [{ name: 'All' }, ...days].map((item, idx) => {
+          return (
+            <MenuItem key={idx} value={item.id}>
+              <span className={classes.dropdownListItem}>{item.name || ''}</span>
+            </MenuItem>
+          )
+        })
+      }
+      <MenuItem key={'custom'} value={'custom'} onClick={() => { setOpenDialog(true) }}>
+        <span className={classes.dropdownListItem}>{startDate !== "-" && startDate !== null && endDate !== null && !trackDateFilterOpen ? moment(startDate).format("DD/MM/YYYY") + " - " + moment(endDate).format("DD/MM/YYYY") : "Custom"}</span>
+      </MenuItem>
+    </Select>
+  </FormControl>
+
+  const startDateRange = <TextField
+    id="date"
+    label="From"
+    type="date"
+    variant="outlined"
+    className={classes.textFieldRange}
+    InputLabelProps={{
+      shrink: true,
+    }}
+    fullWidth
+    inputProps={{ max: endDate ? endDate : dividerDateFormatForFilter(Date.now()) }}
+    defaultValue={startDate}
+    value={startDate}
+    onChange={(e) => setStartDate(e.target.value)}
+    margin="dense"
+  />
+
+  const endDateRange = <TextField
+    id="date"
+    label="To"
+    type="date"
+    variant="outlined"
+    className={classes.textFieldRange}
+    InputLabelProps={{
+      shrink: true,
+    }}
+    fullWidth
+    inputProps={{ min: startDate, max: dividerDateFormatForFilter(Date.now()) }}
+    defaultValue={endDate}
+    value={endDate}
+    onChange={(e) => setEndDate(e.target.value)}
+    margin="dense"
+  />
 
   // status filter
   const statusSelect = <SelectDropdown icon={<MoreHorizIcon fontSize="small" />} type="Status" name="Select Status" list={[{ name: 'All' }, ...filterStatus]} selectedType={selectedFilterStatus} setSelectedType={setSelectedFilterStatus} setPage={setPage} />
 
-  const headerButtons = [statusSelect, searchInput, addDispatchOrderButton, addBulkProductsButton, exportButton, deleteDispatchOrderModal,];
+  const headerButtons = [statusSelect, searchInput, addDispatchOrderButton, addBulkProductsButton, daysSelect, exportButton, deleteDispatchOrderModal,];
 
   return (
     <Paper className={classes.root}>
@@ -509,6 +624,27 @@ export default function DispatchOrderView() {
         </Grid>
       </Grid>
       <MessageSnackbar showMessage={showMessage} type={messageType} />
+
+      <Dialog onClose={handleCloseDialog} open={openDialog}>
+        <DialogTitle>Choose Date</DialogTitle>
+        <ListItem button key={'startDate'}>
+          {startDateRange}
+        </ListItem>
+        <ListItem button key={'startDate'}>
+          {endDateRange}
+        </ListItem>
+        <ListItem autoFocus button style={{ justifyContent: 'flex-end' }} >
+          <Button variant="contained" color="primary"
+            disabled={!startDate || !endDate}
+            onClick={() => {
+              setSelectedDateRange(true)
+              setOpenDialog(false)
+              setReRender(!reRender)
+            }}>
+            OK
+          </Button>
+        </ListItem>
+      </Dialog>
     </Paper>
   );
 }
