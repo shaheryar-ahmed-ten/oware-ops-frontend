6bbb39e7ec8c390f555ae9f18a4b7c6f2bd139f1
Muhammad Hamza <muhammad.hamza@experia.ai>
2021-11-11 14:32:46 +0500

    product category clear

--- src/views/logistics/ride/AddRideView.js
+++ src/views/logistics/ride/AddRideView.js
@@ -259,7 +259,7 @@ function AddRideView() {
   useEffect(() => {
     setProductName("");
     setProductQuantity("");
-    setProductCategoryId(null);
+    setProductCategoryId("");
   }, [products]);
 
   useEffect(() => {
@@ -1190,7 +1190,7 @@ function AddRideView() {
               <FormControl margin="dense" fullWidth={true} variant="outlined">
                 <Autocomplete
                   id="productCategories"
-                  key={productCategories}
+                  key={productCategoryId}
                   options={productCategories}
                   renderInput={(params) => <TextField {...params} label="Product Category" variant="outlined" />}
                   getOptionLabel={(productCategory) => productCategory.name || ""}
