68c3f47f13e324ea6fa7cffdd2b2ec852ed9ef0e
Muhammad Hamza <muhammad.hamza@experia.ai>
2021-11-17 13:18:47 +0500

    Actions label

--- src/views/logistics/ride/RideView.js
+++ src/views/logistics/ride/RideView.js
@@ -191,7 +191,7 @@ export default function RideView() {
     },
     {
       id: "actions",
-      label: "",
+      label: "ACTIONS",
       minWidth: 120,
       className: "",
       format: (value, entity) => [
